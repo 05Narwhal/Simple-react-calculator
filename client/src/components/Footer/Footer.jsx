5a536b89dbb62b36f74a91062e0e4ce5c46769e8
05Narwhal <julianaljaz.loy@stanislav.si>
2024-07-11 20:19:01 +0200

    updated

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 import './Footer.css';
+import logo from '../../assets/images/logo.svg';
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; 2021 Calculator App</p>
+    <footer style={{ display:'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <img 
+        src={logo} 
+        alt="JL Studios logo" 
+        style={{ width: '80px', height: '80px', marginRight: '20px' }}
+      />
+      <p>&copy; JL Studios <strong><em>designs</em></strong></p>
     </footer>
   );
 }
